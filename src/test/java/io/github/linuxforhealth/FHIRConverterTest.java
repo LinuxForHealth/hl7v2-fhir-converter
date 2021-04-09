8a1a16a1140f0089312e1763a740918b60693cb7
dangelis <dangelis@gmail.com>
2021-04-09 13:16:58 -0400

    Refactor/clean up for Medication unit test

--- src/test/java/io/github/linuxforhealth/FHIRConverterTest.java
+++ src/test/java/io/github/linuxforhealth/FHIRConverterTest.java
@@ -125,8 +125,6 @@ public class FHIRConverterTest {
 
   }
 
-
-
   @Test
   public void test_invalid_message_throws_error() throws IOException {
     String hl7message = "some text";
@@ -137,8 +135,6 @@ public class FHIRConverterTest {
 
   }
 
-
-
   @Test
   public void test_blank_message_throws_error() throws IOException {
     String hl7message = "";
@@ -148,102 +144,6 @@ public class FHIRConverterTest {
     ftv.convert(hl7message);
 
   }
-
-  @Test
-  public void test_omp() throws IOException, HL7Exception {
-    String vmsg = "MSH|^~\\&|SendTest1|Sendfac1|Receiveapp1|Receivefac1|200603081747|security|OMP^O09^OMP_O09|1|P^I|2.6|\r" +
-            "PID|||555444222111^^^MPI&GenHosp^MR||smith^john||19600614|M||C|99 Oakland #106^^Toronto^ON^44889||||||||343132266|||N\r" +
-            "PD1|S|A|patientpfacisXON|primarycareXCN||||F|Y|duppatient|pubcode|Y|20060316|placeofworshipXON|DNR|A|20060101|20050101|USA||ACT\r" +
-            "NTE|1|P|comment after PD1\r" +
-            "PV1||I|6N^1234^A^GENHOS||||0100^ANDERSON,CARL|0148^ADDISON,JAMES||SUR|||||||0148^ANDERSON,CARL|S|1400|A|||||||||||||||||||SF|K||||199501102300\r" +
-            "PV2|priorpendingpoint|accomcode|admitreason|transferreason|patientvalu|patientvaluloc|PH|20060315|20060316|2|4|visit description|referral1~referral2|20050411|Y|P|20060415|FP|Y|1|F|Y|clinicorgname|AI|2|20060316|05|20060318|20060318|chargecode|RC|Y|20060315|Y|Y|Y|Y|P|K|AC|A|A|N|Y|DNR|20060101|20060401|200602140900|O\r" +
-            "IN1|1|insuranceplanA|insurancecoB\r" +
-            "IN2|Insuredempid|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||Y|N|Y\r" +
-            "GT1|6357||gname|gspousename|gaddress|||20060308|F|GT||111-22-3456|20060101|20060101|23|gempname|||||||||N||||||||M\r" +
-            "AL1|1|DA|allergydescription|SV|allergyreasonxx\r" +
-            "ORC|NW|1000^OE|9999999^RX|||E|40^QID^D10^^^R\r" +
-            "TQ1||||||30^M|199401060930|199401061000\r" +
-            "TQ2|1|S|relatedplacer|relatedfiller|placergroupnumber|EE|*|10^min|4|N\r" +
-            "RXO|RX2111^Polycillin 500 mg TAB|500||MG|reqdosgform|providerspharm~prpharm2|providersadmin1~providersadmin2||G||40|dispenseunits|2|orderDEAisXCN|pharmverifidXCN|Y|day|3|units|indication|rate|rateunits\r" +
-            "NTE|1|P|comment after RXO\r" +
-            "RXR|^PO|\r" +
-            "RXC|B|D5/.45NACL|1000|ML\r" +
-            "NTE|1|P|comment after RXC\r" +
-            "OBX|1|TX|^hunchback|1|Increasing||||||S\r" +
-            "NTE|1|P|comment after OBX\r" +
-            "FT1|1|||199805291115||CG|00378112001^VerapamilHydrochloride 120 mgTAB^NDC|||1|5&USD^TP\r" +
-            "BLG|D|CH|accountid|01";
-
-//    HapiContext ctx = new DefaultHapiContext();
-//    Message msg = ctx.getGenericParser().parse(vmsg);
-
-    HL7ToFHIRConverter ftv = new HL7ToFHIRConverter();
-
-    ConverterOptions OPTIONS2 =
-            new Builder().withPrettyPrint().build();
-    String json = ftv.convert(vmsg, OPTIONS2);
-
-    System.out.println(json);
-
-  }
-
-  @Test
-  public void test_med() throws IOException {
-    String hl7message = "MSH|^~\\&|PROSLOV|MYHOSPITAL|WHIA|IBM|20170215080000||RDE^O11^RDE_O11|MSGID005520|T|2.6\r" +
-            "PID|1||000054321^^^MRN||COOPER^SHELDON^||19820512|M|||765 SOMESTREET RD UNIT 3A^^PASADENA^LA^|||||S||78654|\r" +
-            "PV1||I|6N^1234^A^GENERAL HOSPITAL2||||0100^ANDERSON,CARL|0148^ADDISON,JAMES||SUR|||||||0148^ANDERSON,CARL|S|8846511|A|||||||||||||||||||SF|K||||20170215080000\r" +
-            "PV2|priorpendingpoint|accomcode|admitreason^Prescribed Medications|transferreason|patientvalu|patientvaluloc|PH|20170315|20060316|2|4|visit description|referral1~referral2|20170411|Y|P|20170415|FP|Y|1|F|Y|General Hospital^^^^^^^GH|AI|2|20170316|05|20170318|20170318|chargecode|RC|Y|20170315|Y|Y|Y|Y|P|K|AC|A|A|N|Y|DNR|20170101|20170401|201702140900|O\r" +
-            "ORC|NW|PON001^OE|CD2017071101^RX|||E|10^BID^D4^^^R||20170215080000\r" +
-            "TQ1|||BID|||10^MG|20170215080000|20170228080000\r" +
-            "RXO|RX700001^DOCUSATE SODIUM 100 MG CAPSULE|100||mg|||||G||10||5|\r" +
-            "RXR|^MTH\r" +
-            "RXC|B|Ampicillin 250|250|MG\r" +
-            "RXE|^^^20170923230000^^R|999^Ampicillin 250 MG TAB^NDC|100||mg|123^test^ABC||||10||5|" +
-            "TQ1|||BID|||10^MG|20170215080000|20170228080000\r" +
-            "RXR|^MTH\r" +
-            "ORC|NW|F700002^OE|P100002^RX|||E|40^QID^D10^^^R||20170215080000\r" +
-            "TQ1|||QID|||40^MG|20170215080000|20170222080000\r" +
-            "RXO|RX700002^colchicine 0.6 mg capsule|0.6||mg|||||G||40||2|||Y|day|3|units|indication|rate|rateunits\r" +
-            "RXR|^NS\r" +
-            "RXC|B|D5/.45NACL|1000|ML\r" +
-            "RXE|^^^20170923230000^^R|999^D5/.45NACL|1000||ml|||||40||2|\r" +
-            "TQ1|||QID|||40^MG|20170215080000|20170222080000\r" +
-            "RXR|^NS\r" +
-            "ORC|NW|F700003^OE|P100003^RX|||E|20^QSHIFT^D10^^^R||20170202080000\r" +
-            "TQ1|||QSHIFT|||20^MG|20170202080000|20170204080000\r" +
-            "RXO|RX700003^thyroxine 0.05 MG|0.05||mg|||||G||20||2|||Y|day|3|units|indication|rate|rateunits\r" +
-            "RXR|^PR\r" +
-            "RXE|^^^20170923230000^^R|999^thyroxine 0.05 MG|0.05||mg|||||20||2|\r" +
-            "TQ1|||QSHIFT|||20^MG|20170202080000|20170204080000\r" +
-            "RXR|^PR\r" +
-            "ORC|NW|F700004^OE|P100004^RX|||E|5^QHS^D4^^^R||20170125080000\r" +
-            "TQ1|||QHS|||5^ML|20170125080000|20170325080000\r" +
-            "RXO|RX700004^metformin 850 ml orally|850||ml|||||G||5||5|\r" +
-            "RXR|^PO\r" +
-            "RXE|^^^20170923230000^^R|999^metformin 850 ml|850||ml|||||3||2|\r" +
-            "TQ1|||QHS|||5^ML|20170125080000|20170325080000\r" +
-            "RXR|^PO\r" +
-            "ORC|NW|F700005^OE|P100005^RX|||E|8^TID^D3^^^R||20170125080000\r" +
-            "TQ1|||TID|||8^MG|20170125080000|20171125230000\r" +
-            "RXO|RX700005^OxyContin 20 MG CAPSULE|20||mg|||||G||8||3|\r" +
-            "RXR|^EP\r" +
-            "RXE|^^^20170923230000^^R|999^OxyContin 20 MG CAPSULE|20||mg|||||5||3|\r" +
-            "TQ1|||TID|||8^MG|20170125080000|20171125230000\r" +
-            "RXR|^EP";
-
-
-    HL7ToFHIRConverter ftv = new HL7ToFHIRConverter();
-
-    ConverterOptions OPTIONS2 =
-            new Builder().withPrettyPrint().build();
-    String json = ftv.convert(hl7message, OPTIONS2);
-
-    System.out.println(json);
-
-  }
-
-
-
 
   @Test
   public void test_VXU_V04_message() {
@@ -267,46 +167,43 @@ public class FHIRConverterTest {
 
     String json = ftv.convert(hl7VUXmessageRep, OPTIONS);
 
-    System.out.println(json);
+    FHIRContext context = new FHIRContext();
+    IBaseResource bundleResource = context.getParser().parseResource(json);
+    assertThat(bundleResource).isNotNull();
+    Bundle b = (Bundle) bundleResource;
+    assertThat(b.getType()).isEqualTo(BundleType.COLLECTION);
+    assertThat(b.getId()).isNotNull();
+    assertThat(b.getMeta().getLastUpdated()).isNotNull();
 
-//    FHIRContext context = new FHIRContext();
-//    IBaseResource bundleResource = context.getParser().parseResource(json);
-//    assertThat(bundleResource).isNotNull();
-//    Bundle b = (Bundle) bundleResource;
-//    assertThat(b.getType()).isEqualTo(BundleType.COLLECTION);
-//    assertThat(b.getId()).isNotNull();
-//    assertThat(b.getMeta().getLastUpdated()).isNotNull();
-//
-//    List<BundleEntryComponent> e = b.getEntry();
-//    List<Resource> patientResource =
-//        e.stream().filter(v -> ResourceType.Patient == v.getResource().getResourceType())
-//            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
-//    assertThat(patientResource).hasSize(1);
-//    List<Resource> messageHeader =
-//        e.stream().filter(v -> ResourceType.MessageHeader == v.getResource().getResourceType())
-//            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
-//    assertThat(messageHeader).hasSize(1);
-//
-//    List<Resource> encounterResource =
-//        e.stream().filter(v -> ResourceType.Encounter == v.getResource().getResourceType())
-//            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
-//    assertThat(encounterResource).hasSize(1);
-//    List<Resource> obsResource =
-//        e.stream().filter(v -> ResourceType.Immunization == v.getResource().getResourceType())
-//            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
-//    assertThat(obsResource).hasSize(1);
-//    List<Resource> pracResource =
-//        e.stream().filter(v -> ResourceType.Practitioner == v.getResource().getResourceType())
-//            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
-//    assertThat(pracResource).hasSize(1);
-//
-//    List<Resource> organizationRes =
-//        e.stream().filter(v -> ResourceType.Organization == v.getResource().getResourceType())
-//            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
-//    assertThat(organizationRes).hasSize(2);
+    List<BundleEntryComponent> e = b.getEntry();
+    List<Resource> patientResource =
+        e.stream().filter(v -> ResourceType.Patient == v.getResource().getResourceType())
+            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
+    assertThat(patientResource).hasSize(1);
+    List<Resource> messageHeader =
+        e.stream().filter(v -> ResourceType.MessageHeader == v.getResource().getResourceType())
+            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
+    assertThat(messageHeader).hasSize(1);
 
+    List<Resource> encounterResource =
+        e.stream().filter(v -> ResourceType.Encounter == v.getResource().getResourceType())
+            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
+    assertThat(encounterResource).hasSize(1);
+    List<Resource> obsResource =
+        e.stream().filter(v -> ResourceType.Immunization == v.getResource().getResourceType())
+            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
+    assertThat(obsResource).hasSize(1);
+    List<Resource> pracResource =
+        e.stream().filter(v -> ResourceType.Practitioner == v.getResource().getResourceType())
+            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
+    assertThat(pracResource).hasSize(1);
 
+    List<Resource> organizationRes =
+        e.stream().filter(v -> ResourceType.Organization == v.getResource().getResourceType())
+            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
+    assertThat(organizationRes).hasSize(2);
   }
+
   private void verifyResult(String json, BundleType expectedBundleType) {
     verifyResult(json, expectedBundleType, true);
   }
@@ -352,7 +249,5 @@ public class FHIRConverterTest {
       assertThat(messageHeader).hasSize(1);
     }
   }
-
-
 
 }
