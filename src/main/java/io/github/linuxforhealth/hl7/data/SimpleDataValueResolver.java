b889d382c514ce41ecca8b78f4eff3f09cb87538
jared.grant@ibm.com <jared.grant@ibm.com>
2021-06-08 10:07:46 -0400

    default to unknown if status not specified and unit test

--- src/main/java/io/github/linuxforhealth/hl7/data/SimpleDataValueResolver.java
+++ src/main/java/io/github/linuxforhealth/hl7/data/SimpleDataValueResolver.java
@@ -309,7 +309,7 @@ public class SimpleDataValueResolver {
         if (code != null) {
           return code;
         } else {
-          return null;
+          return "unknown";
         }
 
       };
