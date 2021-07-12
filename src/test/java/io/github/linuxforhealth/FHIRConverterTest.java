e741a082bed804ba23f3c15df5241f8c5cb00311
jared.grant@ibm.com <jared.grant@ibm.com>
2021-07-12 15:25:49 -0400

    add a test

--- src/test/java/io/github/linuxforhealth/FHIRConverterTest.java
+++ src/test/java/io/github/linuxforhealth/FHIRConverterTest.java
@@ -103,12 +103,14 @@ public class FHIRConverterTest {
         + "PV1|1|ff|yyy|EL|ABC||200^ATTEND_DOC_FAMILY_TEST^ATTEND_DOC_GIVEN_TEST|201^REFER_DOC_FAMILY_TEST^REFER_DOC_GIVEN_TEST|202^CONSULTING_DOC_FAMILY_TEST^CONSULTING_DOC_GIVEN_TEST|MED|||||B6|E|272^ADMITTING_DOC_FAMILY_TEST^ADMITTING_DOC_GIVEN_TEST||48390|||||||||||||||||||||||||201409122200|20150206031726\r"
         + "OBX|1|TX|1234||ECHOCARDIOGRAPHIC REPORT||||||F|||||2740^TRDSE^Janetary~2913^MRTTE^Darren^F~3065^MGHOBT^Paul^J~4723^LOTHDEW^Robert^L|\r"
         + "AL1|1|DRUG|00000741^OXYCODONE||HYPOTENSION\r" 
-        + "AL1|2|DRUG|00001433^TRAMADOL||SEIZURES~VOMITING\r"
-        + "PRB|AD|200603150625|aortic stenosis|53692||2||200603150625";
+        + "AL1|2|DRUG|00001433^TRAMADOL||SEIZURES~VOMITING\r";
 
+    System.out.println(hl7message);
     HL7ToFHIRConverter ftv = new HL7ToFHIRConverter();
     exceptionRule.expect(UnsupportedOperationException.class);
-    ftv.convert(hl7message);
+
+    String json = ftv.convert(hl7message);
+    System.out.println(json);
 
   }
 
@@ -144,6 +146,33 @@ public class FHIRConverterTest {
     assertThat(status).isEqualTo(DiagnosticReport.DiagnosticReportStatus.UNKNOWN);
   }
 
+  @Test
+  public void test_allergy_Criticality(){
+    String hl7message = "MSH|^~\\&|SE050|050|PACS|050|20120912011230||ADT^A01|102|T|2.6|||AL|NE\r"
+            + "PID|0010||PID1234^5^M11^A^MR^HOSP~1234568965^^^USA^SS||DOE^JOHN^A^||19800202|F||W|111 TEST_STREET_NAME^^TEST_CITY^NY^111-1111^USA||(905)111-1111|||S|ZZ|12^^^124|34-13-312||||TEST_BIRTH_PLACE\r"
+            + "AL1|1|DRUG|00000741^OXYCODONE||HYPOTENSION\r";
+    HL7ToFHIRConverter ftv = new HL7ToFHIRConverter();
+    String json = ftv.convert(hl7message, OPTIONS);
+    System.out.println(json);
+
+    FHIRContext context = new FHIRContext();
+    IBaseResource bundleResource = context.getParser().parseResource(json);
+    assertThat(bundleResource).isNotNull();
+    Bundle b = (Bundle) bundleResource;
+    List<BundleEntryComponent> e = b.getEntry();
+    List<Resource> allergy = e.stream()
+            .filter(v -> ResourceType.AllergyIntolerance == v.getResource().getResourceType())
+            .map(BundleEntryComponent::getResource).collect(Collectors.toList());
+    assertThat(allergy).hasSize(1);
+
+    String s = context.getParser().encodeResourceToString(allergy.get(0));
+    Class<? extends IBaseResource> klass = AllergyIntolerance.class;
+    AllergyIntolerance resource = (AllergyIntolerance) context.getParser().parseResource(klass, s);
+
+
+    assertThat(resource.hasCriticality()).isFalse();
+
+  }
   @Test
   public void test_dosage_output() throws  IOException {
 String hl7message =
