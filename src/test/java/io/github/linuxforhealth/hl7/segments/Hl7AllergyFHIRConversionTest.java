5c2d0945ab2ee88388f1610abad020575027f57f
Lisa S. Wellman <peace@us.ibm.com>
2021-07-15 13:36:10 -0500

    map AL1-6 to AllergyIntolerance.onset, not reaction.onset

--- src/test/java/io/github/linuxforhealth/hl7/segments/Hl7AllergyFHIRConversionTest.java
+++ src/test/java/io/github/linuxforhealth/hl7/segments/Hl7AllergyFHIRConversionTest.java
@@ -5,58 +5,65 @@
  */
 package io.github.linuxforhealth.hl7.segments;
 
-import com.google.common.collect.Lists;
-import io.github.linuxforhealth.api.ResourceModel;
-import io.github.linuxforhealth.fhir.FHIRContext;
-import io.github.linuxforhealth.hl7.HL7ToFHIRConverter;
-import io.github.linuxforhealth.hl7.message.HL7FHIRResourceTemplate;
-import io.github.linuxforhealth.hl7.message.HL7FHIRResourceTemplateAttributes;
-import io.github.linuxforhealth.hl7.message.HL7MessageModel;
-import io.github.linuxforhealth.hl7.resource.ResourceReader;
-import io.github.linuxforhealth.hl7.segments.util.AllergyUtils;
-import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.r4.model.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNull;
+
+import java.util.Date;
+
+import org.hl7.fhir.r4.model.AllergyIntolerance;
+import org.hl7.fhir.r4.model.DateTimeType;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import io.github.linuxforhealth.hl7.segments.util.AllergyUtils;
 
 public class Hl7AllergyFHIRConversionTest {
 
-  @Rule
-  public ExpectedException exceptionRule = ExpectedException.none();
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
 
+    @Test
+    public void test_allergy_single() {
+        String hl7message = "MSH|^~\\&|SE050|050|PACS|050|20120912011230||ADT^A01|102|T|2.6|||AL|NE\r"
+                + "PID|0010||PID1234^5^M11^A^MR^HOSP~1234568965^^^USA^SS||DOE^JOHN^A^||19800202|F||W|111 TEST_STREET_NAME^^TEST_CITY^NY^111-1111^USA||(905)111-1111|||S|ZZ|12^^^124|34-13-312||||TEST_BIRTH_PLACE\r"
+                + "AL1|1|DA|^PENICILLIN|MI|PRODUCES HIVES~RASH|MI\r" //
+                + "AL1|2|AA|^CAT DANDER|SV";
 
-  @Test
-  public void test_allergy_single() {
-    String hl7message = "MSH|^~\\&|SE050|050|PACS|050|20120912011230||ADT^A01|102|T|2.6|||AL|NE\r"
-            + "PID|0010||PID1234^5^M11^A^MR^HOSP~1234568965^^^USA^SS||DOE^JOHN^A^||19800202|F||W|111 TEST_STREET_NAME^^TEST_CITY^NY^111-1111^USA||(905)111-1111|||S|ZZ|12^^^124|34-13-312||||TEST_BIRTH_PLACE\r"
-            + "AL1|1|DA|^PENICILLIN|MI|PRODUCES HIVES~RASH|MI\r" //
-            + "AL1|2|AA|^CAT DANDER|SV";
+        AllergyIntolerance allergy = AllergyUtils.createAllergyFromHl7Segment(hl7message);
+        assertThat(allergy.getCriticality().toCode()).isEqualTo("low");
+        assertThat(allergy.getCategory().get(0).getCode()).isEqualTo("medication");
+        assertThat(allergy.getCode().getText()).isEqualTo("PENICILLIN");
+        assertThat(allergy.getReaction().get(0).getManifestation()).extracting(m -> m.getText())
+                .containsExactlyInAnyOrder("PRODUCES HIVES", "RASH");
+    }
 
-    AllergyIntolerance allergy =  AllergyUtils.createAllergyFromHl7Segment(hl7message);
-    assertThat(allergy.getCriticality().toCode()).isEqualTo("low");
-    assertThat(allergy.getCategory().get(0).getCode()).isEqualTo("medication");
-    assertThat(allergy.getCode().getText()).isEqualTo("PENICILLIN");
-    assertThat(allergy.getReaction().get(0).getManifestation()).extracting(m -> m.getText())
-            .containsExactlyInAnyOrder("PRODUCES HIVES", "RASH");
+    @Test
+    public void test_allergy_no_severity() {
+        String hl7message = "MSH|^~\\&|SE050|050|PACS|050|20120912011230||ADT^A01|102|T|2.6|||AL|NE\r"
+                + "PID|0010||PID1234^5^M11^A^MR^HOSP~1234568965^^^USA^SS||DOE^JOHN^A^||19800202|F||W|111 TEST_STREET_NAME^^TEST_CITY^NY^111-1111^USA||(905)111-1111|||S|ZZ|12^^^124|34-13-312||||TEST_BIRTH_PLACE\r"
+                + "AL1|1|DRUG|00000741^OXYCODONE||HYPOTENSION\r";
 
+        AllergyIntolerance resource = AllergyUtils.createAllergyFromHl7Segment(hl7message);
+        assertThat(resource.hasCriticality()).isFalse();
+    }
 
-  }
+    @Test
+    /**
+     * Verifies AL1-6 is put into AllergyIntolerance.onsetDateTime; AllergyIntolerance.reaction.onset is not set.
+     */
+    public void test_allergy_onset() {
+        String hl7message = "MSH|^~\\&|SE050|050|PACS|050|20120912011230||ADT^A01|102|T|2.6|||AL|NE\r"
+                + "PID|0010||PID1234||DOE^JANE|||F\r"
+                + "AL1|1|DRUG|00000741^OXYCODONE||HYPOTENSION|20210101\r";
 
-  @Test
-  public void test_allergy_no_severity(){
-    String hl7message = "MSH|^~\\&|SE050|050|PACS|050|20120912011230||ADT^A01|102|T|2.6|||AL|NE\r"
-            + "PID|0010||PID1234^5^M11^A^MR^HOSP~1234568965^^^USA^SS||DOE^JOHN^A^||19800202|F||W|111 TEST_STREET_NAME^^TEST_CITY^NY^111-1111^USA||(905)111-1111|||S|ZZ|12^^^124|34-13-312||||TEST_BIRTH_PLACE\r"
-            + "AL1|1|DRUG|00000741^OXYCODONE||HYPOTENSION\r";
+        AllergyIntolerance allergy = AllergyUtils.createAllergyFromHl7Segment(hl7message);
+        assertThat(allergy.getReaction().get(0).getManifestation()).extracting(m -> m.getText())
+                .containsExactly("HYPOTENSION");
+        Date onsetReaction = allergy.getReaction().get(0).getOnset();
+        assertNull(onsetReaction);
+        DateTimeType onsetAllergy = allergy.getOnsetDateTimeType();
+        assertThat(onsetAllergy.getValueAsString().equals("20210101"));
+    }
 
-    AllergyIntolerance resource =  AllergyUtils.createAllergyFromHl7Segment(hl7message);
-    assertThat(resource.hasCriticality()).isFalse();
-
-  }
 }
