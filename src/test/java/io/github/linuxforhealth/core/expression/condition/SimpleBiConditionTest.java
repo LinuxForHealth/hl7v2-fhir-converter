eb781ccf28defaab8bee1a45a019e5cec489f804
Brian Cragun <cragun@us.ibm.com>
2021-07-20 15:22:59 -0600

    BJC - extended BiCondition tests

--- src/test/java/io/github/linuxforhealth/core/expression/condition/SimpleBiConditionTest.java
+++ src/test/java/io/github/linuxforhealth/core/expression/condition/SimpleBiConditionTest.java
@@ -16,8 +16,10 @@ import io.github.linuxforhealth.core.expression.condition.SimpleBiCondition;
 
 public class SimpleBiConditionTest {
 
+  // Test EQUALS exhaustively
+
   @Test
-  public void simple_condition_is_evaluated_true() {
+  public void simple_EQUALS_condition_is_evaluated_true() {
     String condition = "$var1 EQUALS abc";
     SimpleBiCondition simplecondition =
         (SimpleBiCondition) ConditionUtil.createCondition(condition);
@@ -26,9 +28,8 @@ public class SimpleBiConditionTest {
     assertThat(simplecondition.test(contextVariables)).isTrue();
   }
 
-
   @Test
-  public void simple_condition_with_two_variables_is_evaluated_true() {
+  public void simple_EQUALS_condition_with_two_variables_is_evaluated_true() {
     String condition = "$var1 EQUALS $var2";
     SimpleBiCondition simplecondition =
         (SimpleBiCondition) ConditionUtil.createCondition(condition);
@@ -39,7 +40,7 @@ public class SimpleBiConditionTest {
   }
 
   @Test
-  public void simple_condition_with_two_variables_is_evaluated_false_when_condition_is_not_satisfied() {
+public void simple_EQUALS_condition_with_two_variables_is_evaluated_false_when_condition_is_not_satisfied() {
     String condition = "$var1 EQUALS $var2";
     SimpleBiCondition simplecondition =
         (SimpleBiCondition) ConditionUtil.createCondition(condition);
@@ -49,10 +50,8 @@ public class SimpleBiConditionTest {
     assertThat(simplecondition.test(contextVariables)).isFalse();
   }
 
-
-
   @Test
-  public void simple_condition_is_evaluated_false() {
+  public void simple_EQUALS_condition_is_evaluated_false() {
     String condition = "$var1 EQUALS abc";
     SimpleBiCondition simplecondition =
         (SimpleBiCondition) ConditionUtil.createCondition(condition);
@@ -61,9 +60,8 @@ public class SimpleBiConditionTest {
     assertThat(simplecondition.test(contextVariables)).isFalse();
   }
 
-
   @Test
-  public void simple_condition_is_evaluated_false_when_var_not_found() {
+  public void simple_EQUALS_condition_is_evaluated_false_when_var_not_found() {
     String condition = "$var1 EQUALS abc";
     SimpleBiCondition simplecondition =
         (SimpleBiCondition) ConditionUtil.createCondition(condition);
@@ -71,4 +69,191 @@ public class SimpleBiConditionTest {
 
     assertThat(simplecondition.test(contextVariables)).isFalse();
   }
+
+  // NOT_EQUALS tested exhaustively
+
+  @Test
+  public void simple_NOT_EQUALS_condition_is_evaluated_true() {
+    String condition = "$var1 NOT_EQUALS abc";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("abcd"));
+    assertThat(simplecondition.test(contextVariables)).isTrue();
+  }
+
+  @Test
+  public void simple_NOT_EQUALS_condition_with_two_variables_is_evaluated_true() {
+    String condition = "$var1 NOT_EQUALS $var2";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("abc"));
+    contextVariables.put("var2", new SimpleEvaluationResult("abcd"));
+    assertThat(simplecondition.test(contextVariables)).isTrue();
+  }
+
+  @Test
+public void simple_NOT_EQUALS_condition_with_two_variables_is_evaluated_false_when_condition_is_not_satisfied() {
+    String condition = "$var1 NOT_EQUALS $var2";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("abc"));
+    contextVariables.put("var2", new SimpleEvaluationResult("abc"));  // Because the same, fails NOT_EQUALS
+    assertThat(simplecondition.test(contextVariables)).isFalse();
+  }
+
+  @Test
+  public void simple_NOT_EQUALS_condition_is_evaluated_false() {
+    String condition = "$var1 NOT_EQUALS abc";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("abc")); // Because the same, NOT_EQUALS false
+    assertThat(simplecondition.test(contextVariables)).isFalse();
+  }
+
+  @Test
+  public void simple_NOT_EQUALS_condition_is_evaluated_false_when_var_not_found() {
+    String condition = "$var1 NOT_EQUALS abc";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+
+    assertThat(simplecondition.test(contextVariables)).isFalse();
+  }
+
+  // CONTAINS test one true and one false
+
+  @Test
+  public void simple_CONTAINS_condition_is_evaluated_true() {
+    String condition = "$var1 CONTAINS bcd";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("abcde"));
+    assertThat(simplecondition.test(contextVariables)).isTrue();
+  }
+
+  @Test
+  public void simple_CONTAINS_condition_is_evaluated_false() {
+    String condition = "$var1 CONTAINS def";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("abcde"));
+    assertThat(simplecondition.test(contextVariables)).isFalse();
+  }
+
+  // NOT_CONTAINS test one true and one false
+
+  @Test
+  public void simple_NOT_CONTAINS_condition_is_evaluated_true() {
+    String condition = "$var1 NOT_CONTAINS abc";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("defgh"));
+    assertThat(simplecondition.test(contextVariables)).isTrue();
+  }
+
+  @Test
+  public void simple_NOT_CONTAINS_condition_is_evaluated_false() {
+    String condition = "$var1 NOT_CONTAINS abc";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("abcdef"));
+    assertThat(simplecondition.test(contextVariables)).isFalse();
+  }
+
+  // ENDS_WITH test one true and one false
+
+  @Test
+  public void simple_ENDS_WITH_condition_is_evaluated_true() {
+    String condition = "$var1 ENDS_WITH xyz";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("uvwxyz"));
+    assertThat(simplecondition.test(contextVariables)).isTrue();
+  }
+
+  @Test
+  public void simple_ENDS_WITH_condition_is_evaluated_false() {
+    String condition = "$var1 ENDS_WITH abc";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("uvwxyz"));
+    assertThat(simplecondition.test(contextVariables)).isFalse();
+  }
+
+  // NOT_ENDS_WITH test one true and one false
+
+  @Test
+  public void simple_NOT_ENDS_WITH_condition_is_evaluated_true() {
+    String condition = "$var1 NOT_ENDS_WITH abc";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("uvwxyz"));
+    assertThat(simplecondition.test(contextVariables)).isTrue();
+  }
+
+  @Test
+  public void simple_NOT_ENDS_WITH_condition_is_evaluated_false() {
+    String condition = "$var1 NOT_ENDS_WITH xyz";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("uvwxyz"));
+    assertThat(simplecondition.test(contextVariables)).isFalse();
+  }
+
+  // STARTS_WITH test one true and one false
+
+  @Test
+  public void simple_STARTS_WITH_condition_is_evaluated_true() {
+    String condition = "$var1 STARTS_WITH abc";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("abcdefg"));
+    assertThat(simplecondition.test(contextVariables)).isTrue();
+  }
+
+  @Test
+  public void simple_STARTS_WITH_condition_is_evaluated_false() {
+    String condition = "$var1 STARTS_WITH abc";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("uvwxyz"));
+    assertThat(simplecondition.test(contextVariables)).isFalse();
+  }
+
+  // NOT_STARTS_WITH test one true and one false
+
+  @Test
+  public void simple_NOT_STARTS_WITH_condition_is_evaluated_true() {
+    String condition = "$var1 NOT_STARTS_WITH abc";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("uvwxyz"));
+    assertThat(simplecondition.test(contextVariables)).isTrue();
+  }
+
+  @Test
+  public void simple_NOT_STARTS_WITH_condition_is_evaluated_false() {
+    String condition = "$var1 NOT_STARTS_WITH abc";
+    SimpleBiCondition simplecondition =
+        (SimpleBiCondition) ConditionUtil.createCondition(condition);
+    Map<String, EvaluationResult> contextVariables = new HashMap<>();
+    contextVariables.put("var1", new SimpleEvaluationResult("abcdefg"));
+    assertThat(simplecondition.test(contextVariables)).isFalse();
+  }
+
 }
