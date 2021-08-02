33f59390733b03b7694a3b520f4f5d9ddd10efb1
Joel Maner <xpander53@yahoo.com>
2021-08-02 13:14:57 -0400

    add back test case

--- src/test/java/io/github/linuxforhealth/hl7/data/Hl7RelatedGeneralUtilsTest.java
+++ src/test/java/io/github/linuxforhealth/hl7/data/Hl7RelatedGeneralUtilsTest.java
@@ -81,13 +81,13 @@ public class Hl7RelatedGeneralUtilsTest {
   }
 
 
-//  @Test
-  // public void test_date_diff_valid_values_no_min() {
+ @Test
+  public void test_date_diff_valid_values_no_min() {
 
-  //   Long diff = Hl7RelatedGeneralUtils.diffDateMin("2007-11-05",
-  //       "2007-11-04T01:33:06.345+09:00");
-  //   assertThat(diff).isNull();
-  // }
+    Long diff = Hl7RelatedGeneralUtils.diffDateMin("2007-11-05",
+        "2007-11-04T01:33:06.345+09:00");
+    assertThat(diff).isNull();
+  }
 
 
   @Test
