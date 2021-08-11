df13e913f80740f9ac37aef1791abf13b290c746
Joel Maner <xpander53@yahoo.com>
2021-08-11 17:25:10 -0400

    Add evn unit tests

--- src/test/java/io/github/linuxforhealth/FHIRConverterTest.java
+++ src/test/java/io/github/linuxforhealth/FHIRConverterTest.java
@@ -353,4 +353,71 @@ String hl7message =
     }
   }
 
+  @Test
+  public void validate_evn_segment () {
+
+    String hl7message = "MSH|^~\\&|||||||ADT^A01^ADT_A01|64322|P|2.6|123|456|ER|AL|USA|ASCII|en|2.6||||||\r" +
+                        "EVN||||851||20210319134735|\r"+
+                        "PV1|1|I||R|||||||||R|1||||||||||||||||||||||||||||||||||||||";                    
+                        
+
+    HL7ToFHIRConverter ftv = new HL7ToFHIRConverter();
+    String json = ftv.convert(hl7message , OPTIONS);
+    assertThat(json).isNotBlank();
+
+    FHIRContext context = new FHIRContext(true, false);
+    IBaseResource bundleResource = context.getParser().parseResource(json);
+    assertThat(bundleResource).isNotNull();
+    Bundle b = (Bundle) bundleResource;
+    List<BundleEntryComponent> e = b.getEntry();
+
+    //Find the encounter from the FHIR bundle.
+    List<Resource> encounterResource = e.stream()
+        .filter(v -> ResourceType.Encounter == v.getResource().getResourceType()).map(BundleEntryComponent::getResource)
+        .collect(Collectors.toList());
+    assertThat(encounterResource).hasSize(1);
+
+    //ENV.4 is used for reasonCode
+    String reasonCode = encounterResource.get(0).getNamedProperty("reasonCode").getValues().get(0).getNamedProperty("text").getValues().get(0).toString();
+    assertThat(reasonCode).isEqualTo("851");
+
+    //EVN.6 is used for start period if there is no PV1.44 
+    String startPeriod = encounterResource.get(0).getNamedProperty("period").getValues().get(0).getNamedProperty("start").getValues().get(0).toString();
+    assertThat(startPeriod).isEqualTo("DateTimeType[2021-03-19T13:47:35+08:00]");
+  }
+
+@Test
+public void validate_evn_segment_no_period_override() {
+
+  String hl7message = "MSH|^~\\&|||||||ADT^A01^ADT_A01|64322|P|2.6|123|456|ER|AL|USA|ASCII|en|2.6||||||\r" +
+                      "EVN||||7525||20210319134735|\r"+
+                      "PV1|1|I||R|||||||||R|1||||||||||||||||||||||||||||||200603150624|200603150625|||||||";                    
+                      
+
+  HL7ToFHIRConverter ftv = new HL7ToFHIRConverter();
+  String json = ftv.convert(hl7message , OPTIONS);
+  assertThat(json).isNotBlank();
+
+  FHIRContext context = new FHIRContext(true, false);
+  IBaseResource bundleResource = context.getParser().parseResource(json);
+  assertThat(bundleResource).isNotNull();
+  Bundle b = (Bundle) bundleResource;
+  List<BundleEntryComponent> e = b.getEntry();
+
+  //Find the encounter from the FHIR bundle.
+  List<Resource> encounterResource = e.stream()
+      .filter(v -> ResourceType.Encounter == v.getResource().getResourceType()).map(BundleEntryComponent::getResource)
+      .collect(Collectors.toList());
+  assertThat(encounterResource).hasSize(1);
+
+  //ENV.4 is used for reasonCode
+  String reasonCode = encounterResource.get(0).getNamedProperty("reasonCode").getValues().get(0).getNamedProperty("text").getValues().get(0).toString();
+  assertThat(reasonCode).isEqualTo("7525");
+
+  //EVN.6 is used for start period if there is no PV1.44 but since we have a PV1.44 it should use that not EVN.6
+  String startPeriod = encounterResource.get(0).getNamedProperty("period").getValues().get(0).getNamedProperty("start").getValues().get(0).toString();
+  assertThat(startPeriod).isNotEqualTo("DateTimeType[2021-03-19T13:47:35+08:00]");
+
+}
+
 }
