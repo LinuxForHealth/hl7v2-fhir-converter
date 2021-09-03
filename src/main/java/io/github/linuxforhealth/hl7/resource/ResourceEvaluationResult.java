7b637462d7f507b801d4309a906b005f94c0a922
Pushpa Bhallamudi <pbhallam@us.ibm.com>
2021-09-03 12:23:56 +0800

    Added support for evaluating exptession at the end
    Signed-off-by: Pushpa Bhallamudi <pbhallam@us.ibm.com>
    Signed-off-by: jared.grant@ibm.com <jared.grant@ibm.com>

--- src/main/java/io/github/linuxforhealth/hl7/resource/ResourceEvaluationResult.java
+++ src/main/java/io/github/linuxforhealth/hl7/resource/ResourceEvaluationResult.java
@@ -4,24 +4,33 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import io.github.linuxforhealth.api.Expression;
 import io.github.linuxforhealth.api.ResourceValue;
 
 public class ResourceEvaluationResult {
 
 
   private List<ResourceValue> additionalResolveValues;
-
-
   private Map<String, Object> resolveValues;
+  private Map<String, Expression> pendingExpressions;
 
 
+  public ResourceEvaluationResult(Map<String, Object> resolveValues) {
+    this(resolveValues, new ArrayList<>(), new HashMap<>());
+  }
 
   public ResourceEvaluationResult(Map<String, Object> resolveValues,
       List<ResourceValue> additionalResolveValues) {
+    this(resolveValues, additionalResolveValues, new HashMap<>());
+  }
+
+  public ResourceEvaluationResult(Map<String, Object> resolveValues,
+      List<ResourceValue> additionalResolveValues, Map<String, Expression> pendingExpressions) {
     this.additionalResolveValues = new ArrayList<>();
     this.additionalResolveValues.addAll(additionalResolveValues);
     this.resolveValues = new HashMap<>();
     this.resolveValues.putAll(resolveValues);
+    this.pendingExpressions = pendingExpressions;
   }
 
 
@@ -33,6 +42,10 @@ public class ResourceEvaluationResult {
 
   public Map<String, Object> getResolveValues() {
     return resolveValues;
+  }
+
+  public Map<String, Expression> getPendingExpressions() {
+    return pendingExpressions;
   }
 
 }
