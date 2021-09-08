d634740f506475e4779f9e784f7a123527f5c759
Brian Cragun <cragun@us.ibm.com>
2021-09-08 17:06:18 -0600

    Issue#206 code review updates
    Signed-off-by: Brian Cragun <cragun@us.ibm.com>
    Signed-off-by: jared.grant@ibm.com <jared.grant@ibm.com>

--- src/main/java/io/github/linuxforhealth/hl7/data/Hl7RelatedGeneralUtils.java
+++ src/main/java/io/github/linuxforhealth/hl7/data/Hl7RelatedGeneralUtils.java
@@ -27,24 +27,42 @@ import io.github.linuxforhealth.hl7.data.date.DateUtil;
 
 public class Hl7RelatedGeneralUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(Hl7RelatedGeneralUtils.class);
-
+    
     private Hl7RelatedGeneralUtils() {
     }
 
+    // IMPORTANT NOTES about Extracting the low and high number.  Description of these numbers is:
     // "When the observation quantifies the amount of a toxic substance, then the upper limit of the range identifies the toxic limit. 
     // If the observation quantifies a drug, the lower limits identify the lower therapeutic bounds and the upper limits represent 
     // the upper therapeutic bounds above which toxic side effects are common.
     // We don't know if it's a toxic substance or a drug, but we understand that:
-    //  1. Two values means we have lower and upper limits.
-    //  2. One value means only the upper limit.
-    //
+    //  1. Two numbers means we have lower and upper limits.
+    //  2. One number means only the upper limit.
+    // NOTE: the current implementation does not pay attention to >, <, -, or other notation.  Just a first (and second) decimal number.
+    // Thus ">0.50" "<0.50" and "-0.50" are all treated the same and a high number is found.
+    // Thus "0.50-2.50" "0.50 2.50" "<0.50 < 2.50" and ">0.50 > 2.50" are all the same, the first is low, the second is high.
+    // This may need to be changed in the future if we find actual data is different.
 
+    // This regex extracts 2 sets of numbers (including decimal points) from a string.  
+    // For example, from the string "1.1 mL - 1.5 mL" the first group is 1.1, and the second group is 1.5
+    // This regex has any number of non-numeric (non greedy), a number, characters, a number, optional additional characters.
+    // ^  beginning of line 
+    // \D*? any number of non-numeric characters (non greedy)
+    // ([\\d.]+) capture as first group a set of digits and period (at least one digit required)
+    // \D* any number of non-numeric characters
+    // ([\\d.]+) capture as second group a set of digits and period (will be empty if not found)
+    // .* any number of characters
+    private static final String REGEX_FIRST_TWO_NUMBERS_AMID_OTHER_TEXT = "^\\D*?([\\d.]+)\\D*([\\d.]*).*";
+    // Compile this into a pattern for reuse
+    private static final Pattern PATTERN_FIRST_TWO_NUMBERS_AMID_OTHER_TEXT = Pattern.compile(REGEX_FIRST_TWO_NUMBERS_AMID_OTHER_TEXT);
+
+    // ExtractLow - see comments above
     public static String extractLow(Object input) {
         String val = Hl7DataHandlerUtil.getStringValue(input);
         if (StringUtils.isNotBlank(val)) {
-            Pattern r = Pattern.compile("^\\D*?([\\d.]+)\\D*([\\d.]*).*");
-            Matcher m = r.matcher(val);
+            Matcher m = PATTERN_FIRST_TWO_NUMBERS_AMID_OTHER_TEXT.matcher(val);
 
+            // Only the low (first) number if there is also a high (second) number
             if (m.find() && !m.group(2).isEmpty()){
                     return m.group(1);
             } 
@@ -53,12 +71,13 @@ public class Hl7RelatedGeneralUtils {
         return null;
     }
 
+    // ExtractHigh - see comments above
     public static String extractHigh(Object input) {
         String val = Hl7DataHandlerUtil.getStringValue(input);
         if (StringUtils.isNotBlank(val)) {
-            Pattern r = Pattern.compile("^\\D*?([\\d.]+)\\D*([\\d.]*).*");
-            Matcher m = r.matcher(val);
+            Matcher m = PATTERN_FIRST_TWO_NUMBERS_AMID_OTHER_TEXT.matcher(val);
 
+            // If one number, it is the high.  If two numbers, the second is high.
             if(m.find()) {
                 if (!m.group(2).isEmpty()){
                     return m.group(2);
