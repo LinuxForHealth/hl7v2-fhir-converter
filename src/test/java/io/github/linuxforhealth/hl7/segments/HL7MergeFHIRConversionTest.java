79b5d55542076f551a836de3c733ea4befdf089f
Joel Maner <xpander53@yahoo.com>
2021-09-11 13:48:55 -0400

    Add first real unit test for merge

--- src/test/java/io/github/linuxforhealth/hl7/segments/HL7MergeFHIRConversionTest.java
+++ src/test/java/io/github/linuxforhealth/hl7/segments/HL7MergeFHIRConversionTest.java
@@ -10,9 +10,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.hl7.fhir.r4.model.Base;
 import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
-import org.hl7.fhir.r4.model.ResourceType;
 import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.ResourceType;
 import org.junit.jupiter.api.Test;
 
 import io.github.linuxforhealth.hl7.segments.util.ResourceUtils;
@@ -21,7 +22,7 @@ public class HL7MergeFHIRConversionTest {
 
     // TBD
     @Test
-    public void validateMerge1() {
+    public void validateMerge() {
 
         String hl7message = "MSH|^~\\&|SENDING_APPLICATION|SENDING_FACILITY|RECEIVING_APPLICATION|RECEIVING_FACILITY|20110613122406637||ADT^A01|1965403220110613122406637|P|2.3||||\r"
         + "EVN|A40|20110613122406637||01\r"
@@ -30,61 +31,66 @@ public class HL7MergeFHIRConversionTest {
 
         List<BundleEntryComponent> e =ResourceUtils.createHl7Segment(hl7message);
 
-        // Find the condition from the FHIR bundle.
+        // Find the patient resources in the FHIR bundle.
         List<Resource> patientResource = e.stream()
         .filter(v -> ResourceType.Patient== v.getResource().getResourceType())
         .map(BundleEntryComponent::getResource).collect(Collectors.toList());
         assertThat(patientResource).hasSize(2);
 
-        // Get condition Resource
-        Resource patient = patientResource.get(0);
+        // Get first patient
+        Resource patientOne = patientResource.get(0);
 
+        // Verify first patient has these fields
+        //  "active": true,
+        //  "link": [
+        //    {
+        //      "other": {
+        //        "reference": "Patient/expiringID-MRG"
+        //      },
+        //      "type": "replaces"
+        //    }
+        //  ]
+
+        assertThat(ResourceUtils.getValueAsString(patientOne, "active")).isEqualTo("true");
+
+        Base linkOne = ResourceUtils.getValue(patientOne, "link");
+        assertThat(ResourceUtils.getValueAsString(linkOne, "type")).isEqualTo("replaces");
+        Base patientOneRef = ResourceUtils.getValue(patientOne, "other");
+        assertThat(ResourceUtils.getValueAsString(patientOneRef, "reference").substring(0, 8)).isEqualTo("Patient/");
+
+        // Get second patient
+        Resource patientTwo = patientResource.get(1);
+
+        // Verify second patient has these fields.
+        //  "active": false,
+        //  "link": [
+        //    {
+        //      "other": {
+        //        "reference": "Patient/survivingID"
+        //      },
+        //      "type": "replaced-by"
+        //    }
+        //  ]
+        assertThat(ResourceUtils.getValueAsString(patientTwo, "active")).isEqualTo("false");
+
+        Base linkTwo = ResourceUtils.getValue(patientTwo, "link");
+        assertThat(ResourceUtils.getValueAsString(linkTwo, "type")).isEqualTo("replaced-by");
+        Base patientTwoRef = ResourceUtils.getValue(patientTwo, "other");
+        assertThat(ResourceUtils.getValueAsString(patientTwoRef, "reference").substring(0, 8)).isEqualTo("Patient/");
+    
     }
 
-    // TBD
-    @Test
-    public void validateMerge2() {
+    // TODO: Add tests for this as well
+    // String hl7message = "MSH|^~\\&|REGADT|MCM|RSP1P8|MCM|200301051530|SEC|ADT^A01|00000003|P|2.5\r"
+    // + "EVN|A40|200301051530\r"
+    // + "PID|||MR1^^^XYZ^MR||MAIDENNAME^EVE\r"
+    // + "MRG|MR2^^^XYZ\r";
 
-        String hl7message = "MSH|^~\\&|REGADT|MCM|RSP1P8|MCM|200301051530|SEC|ADT^A01|00000003|P|2.5\r"
-        + "EVN|A40|200301051530\r"
-        + "PID|||MR1^^^XYZ^MR||MAIDENNAME^EVE\r"
-        + "MRG|MR2^^^XYZ\r";
+    // String hl7message = "MSH|^~\\&|REGADT|MCM|RSP1P8|MCM|200301051530|SEC|ADT^A01|00000003|P|2.5\r"
+    // + "EVN|A40|200301051530\r"
+    // + "PID|||MR1^^^XYZ^MR||EVERYWOMAN^EVE|||||||||||||ACCT3\r"
+    // + "MRG|MR2^^^XYZ||ACCT1\r"
+    // + "PID|||MR1^^^XYZ||EVERYWOMAN^EVE|||||||||||||ACCT4\r"
+    // + "MRG|MR2^^^XYZ||ACCT2\r";
 
-        List<BundleEntryComponent> e =ResourceUtils.createHl7Segment(hl7message);
-
-        // Find the condition from the FHIR bundle.
-        List<Resource> patientResource = e.stream()
-        .filter(v -> ResourceType.Patient== v.getResource().getResourceType())
-        .map(BundleEntryComponent::getResource).collect(Collectors.toList());
-        assertThat(patientResource).hasSize(2);
-
-        // Get condition Resource
-        Resource patient = patientResource.get(0);
-
-    }
-
-    // TBD
-    @Test
-    public void validateMerge3() {
-
-        String hl7message = "MSH|^~\\&|REGADT|MCM|RSP1P8|MCM|200301051530|SEC|ADT^A01|00000003|P|2.5\r"
-        + "EVN|A40|200301051530\r"
-        + "PID|||MR1^^^XYZ^MR||EVERYWOMAN^EVE|||||||||||||ACCT3\r"
-        + "MRG|MR2^^^XYZ||ACCT1\r"
-        + "PID|||MR1^^^XYZ||EVERYWOMAN^EVE|||||||||||||ACCT4\r"
-        + "MRG|MR2^^^XYZ||ACCT2\r";
-
-        List<BundleEntryComponent> e =ResourceUtils.createHl7Segment(hl7message);
-
-        // Find the condition from the FHIR bundle.
-        List<Resource> patientResource = e.stream()
-        .filter(v -> ResourceType.Patient== v.getResource().getResourceType())
-        .map(BundleEntryComponent::getResource).collect(Collectors.toList());
-        assertThat(patientResource).hasSize(2);
-
-        // Get condition Resource
-        Resource patient = patientResource.get(0);
-
-    }
-
-}
+  }
