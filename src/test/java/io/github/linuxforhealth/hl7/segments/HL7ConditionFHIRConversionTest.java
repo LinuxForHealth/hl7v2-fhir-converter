0b9d9f439f304ea9bde2a935e217716891724932
Joel Maner <xpander53@yahoo.com>
2021-09-11 13:03:48 -0400

    comment tweak

--- src/test/java/io/github/linuxforhealth/hl7/segments/HL7ConditionFHIRConversionTest.java
+++ src/test/java/io/github/linuxforhealth/hl7/segments/HL7ConditionFHIRConversionTest.java
@@ -260,7 +260,11 @@ public class HL7ConditionFHIRConversionTest {
 
     // Tests that the Ecnounter has the full aray of condition references in both diagnosis and reasonReference.
     @Test
+<<<<<<< HEAD
     public void validateEncounterMultipleDiagnosesTestingMultipleDiagnosisAndReasonReferences() {
+=======
+    public void validateEncounterMultipleDiagnosesTestingMultipleDiagnosisAndReasonRefernces() {
+>>>>>>> 9997c33 (comment tweak)
 
         String hl7message = "MSH|^~\\&|PROSOLV|SENTARA|WHIA|IBM|20151008111200|S1|ADT^A01^ADT_A01|MSGID000001|T|2.6|10092|PRPA008|AL|AL|100|8859/1|ENGLISH|ARM|ARM5007\r"
         + "EVN|A04|20151008111200|20171013152901|O|OID1006|20171013153621|EVN1009\r"
