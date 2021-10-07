3c540c5f7c5f820ae5b9262166b9398c1318b346
Lisa Dierkhising <lisadier@us.ibm.com>
2021-10-07 12:26:06 -0500

    RDE fixes and increase unit test phase one
    Signed-off-by: Lisa Dierkhising <lisadier@us.ibm.com>

--- src/test/java/io/github/linuxforhealth/hl7/message/Hl7RDEMessageTest.java
+++ src/test/java/io/github/linuxforhealth/hl7/message/Hl7RDEMessageTest.java
@@ -40,6 +40,7 @@ public class Hl7RDEMessageTest {
 		        + "RXA|0|23|20210407191342|20210407191342|DEFAULTMED^cefTRIAXone (ROCEPHIN) 2 g in sodium chloride 0.9 % 50 mL IVPB|||||12345^LNAME^FNAME|BN6T3|100^mL/hr||||||||RateChange||20210407191342\n"
 		        + "ZXC||00409-7335-03^CEFTRIAXONE SODIUM 2 G IJ SOLR^REPNDC\n"
 		        + "ZCL|hcm-prod1862018998731\n";
+                        // TODO: RXA, ZXC, ZCL are not in RDE_o11.
 
       HL7ToFHIRConverter ftv = new HL7ToFHIRConverter();
       String json = ftv.convert(hl7message, OPTIONS);
@@ -48,6 +49,7 @@ public class Hl7RDEMessageTest {
       assertThat(bundleResource).isNotNull();
       Bundle b = (Bundle) bundleResource;
       List<BundleEntryComponent> e = b.getEntry();
+      
       List<Resource> patientResource = e.stream()
               .filter(v -> ResourceType.Patient == v.getResource().getResourceType())
               .map(BundleEntryComponent::getResource).collect(Collectors.toList());
@@ -57,6 +59,8 @@ public class Hl7RDEMessageTest {
               .filter(v -> ResourceType.Encounter == v.getResource().getResourceType())
               .map(BundleEntryComponent::getResource).collect(Collectors.toList());
       assertThat(encounterResource).hasSize(1);
+
+      // TODO: Add check for Medication Request
 
   }
 
@@ -71,6 +75,8 @@ public class Hl7RDEMessageTest {
 		        + "RXE|^Q24H&0600^^20210407191342^^ROU|DEFAULTMED^cefTRIAXone (ROCEPHIN) 2 g in sodium chloride 0.9 % 50 mL IVPB|2||g||||||||12345^LNAME^FNAME\n"
 		        + "RXA|0|23|20210407191342|20210407191342|DEFAULTMED^cefTRIAXone (ROCEPHIN) 2 g in sodium chloride 0.9 % 50 mL IVPB|||||12345^LNAME^FNAME|BN6T3|100^mL/hr||||||||RateChange||20210407191342\n"
 		        + "OBX|1|NM|Most Current Weight^Most current measured weight (actual)||90|kg";
+                        // TODO: RXA is not in RDE_o11 messages
+                        // TODO: Add check for MedicationRequest, ORC and RXE are required, and RXO is optional.  ORC + RXE is repeatable
 		        
       HL7ToFHIRConverter ftv = new HL7ToFHIRConverter();
       String json = ftv.convert(hl7message, OPTIONS);
@@ -79,6 +85,7 @@ public class Hl7RDEMessageTest {
       assertThat(bundleResource).isNotNull();
       Bundle b = (Bundle) bundleResource;
       List<BundleEntryComponent> e = b.getEntry();
+
       List<Resource> patientResource = e.stream()
               .filter(v -> ResourceType.Patient == v.getResource().getResourceType())
               .map(BundleEntryComponent::getResource).collect(Collectors.toList());
@@ -90,6 +97,29 @@ public class Hl7RDEMessageTest {
       assertThat(observationResource).hasSize(1);
   }
 
+  @Test
+  public void test_RDEO11_medrequest_present() throws IOException {
+	  String hl7message =
+		        "MSH|^~\\&|WHI_LOAD_GENERATOR|IBM_TORONTO_LAB||IBM|20210407191342|25739|RDE^O11|MSGID_f209e83f-20db-474d-a7ae-82e5c3894273|T|2.6\n"
+		        + "PID|||1234^^^^MR||DOE^JANE^|||F||||||||||||||||||||||\n"
+		        + "ORC|RE|ACCESSION_62fd10ea-522a-4261-837a-30381a5aa04a|ACCESSION_62fd10ea-522a-4261-837a-30381a5aa04a|3200|||^Every 24 hours&1500^^20210330150000^^ROU||20210407191342|2739^BY^ENTERED|2799^BY^VERIFIED|3122^PROVIDER^ORDERING||(696)901-1300|20210407191342||||||ORDERING FAC NAME|ADDR^^CITY^STATE^ZIP^USA|(515)-290-8888|9999^^CITY^STATE^ZIP^CAN|||||I\n"
+		        + "RXO|DEFAULTMED\n"
+		        + "RXE|^Q24H&0600^^20210407191342^^ROU|DEFAULTMED^cefTRIAXone (ROCEPHIN) 2 g in sodium chloride 0.9 % 50 mL IVPB|2||g||||||||12345^LNAME^FNAME\n";
+
+      HL7ToFHIRConverter ftv = new HL7ToFHIRConverter();
+      String json = ftv.convert(hl7message, OPTIONS);
+      assertThat(json).isNotBlank();
+      IBaseResource bundleResource = context.getParser().parseResource(json);
+      assertThat(bundleResource).isNotNull();
+      Bundle b = (Bundle) bundleResource;
+      List<BundleEntryComponent> e = b.getEntry();
+
+      List<Resource> resourceList = e.stream()
+              .filter(v -> ResourceType.MedicationRequest == v.getResource().getResourceType())
+              .map(BundleEntryComponent::getResource).collect(Collectors.toList());
+      assertThat(resourceList).hasSize(1);
+
+  }
 
   @Test
   public void test_RDEO25_patient_encounter_present() throws IOException {
@@ -120,31 +150,6 @@ public class Hl7RDEMessageTest {
       assertThat(encounterResource).hasSize(1);
 
   }
-
-  @Test
-  public void test_RDEO11_medrequest_present() throws IOException {
-	  String hl7message =
-		        "MSH|^~\\&|WHI_LOAD_GENERATOR|IBM_TORONTO_LAB||IBM|20210407191342|25739|RDE^O11|MSGID_f209e83f-20db-474d-a7ae-82e5c3894273|T|2.6\n"
-		        + "PID|||1234^^^^MR||DOE^JANE^|||F||||||||||||||||||||||\n"
-		        + "ORC|RE|ACCESSION_62fd10ea-522a-4261-837a-30381a5aa04a|ACCESSION_62fd10ea-522a-4261-837a-30381a5aa04a|3200|||^Every 24 hours&1500^^20210330150000^^ROU||20210407191342|2739^BY^ENTERED|2799^BY^VERIFIED|3122^PROVIDER^ORDERING||(696)901-1300|20210407191342||||||ORDERING FAC NAME|ADDR^^CITY^STATE^ZIP^USA|(515)-290-8888|9999^^CITY^STATE^ZIP^CAN|||||I\n"
-		        + "RXO|DEFAULTMED\n"
-		        + "RXE|^Q24H&0600^^20210407191342^^ROU|DEFAULTMED^cefTRIAXone (ROCEPHIN) 2 g in sodium chloride 0.9 % 50 mL IVPB|2||g||||||||12345^LNAME^FNAME\n";
-
-      HL7ToFHIRConverter ftv = new HL7ToFHIRConverter();
-      String json = ftv.convert(hl7message, OPTIONS);
-      assertThat(json).isNotBlank();
-      IBaseResource bundleResource = context.getParser().parseResource(json);
-      assertThat(bundleResource).isNotNull();
-      Bundle b = (Bundle) bundleResource;
-      List<BundleEntryComponent> e = b.getEntry();
-
-      List<Resource> resourceList = e.stream()
-              .filter(v -> ResourceType.MedicationRequest == v.getResource().getResourceType())
-              .map(BundleEntryComponent::getResource).collect(Collectors.toList());
-      assertThat(resourceList).hasSize(1);
-
-  }
-
 
   @Test
   public void test_RDEO25_medrequest_present() throws IOException {
