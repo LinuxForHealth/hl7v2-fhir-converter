8a0d70c0ffd23794e70c7704e6c2f03df35e802d
jared.grant@ibm.com <jared.grant@ibm.com>
2021-10-14 15:55:25 -0400

    RXA.10 test to make sure back up value works
    Signed-off-by: jared.grant@ibm.com <jared.grant@ibm.com>

--- src/test/java/io/github/linuxforhealth/hl7/message/ImmunizationTest.java
+++ src/test/java/io/github/linuxforhealth/hl7/message/ImmunizationTest.java
@@ -89,7 +89,7 @@ public class ImmunizationTest {
         HL7MessageModel message = new HL7MessageModel("VXU_V04", Lists.newArrayList(patient, immunization));
         String hl7VUXmessageRep = "MSH|^~\\&|MYEHR2.5|RI88140101|KIDSNET_IFL|RIHEALTH|20130531||VXU^V04^VXU_V04|20130531RI881401010105|P|2.5.1|||NE|AL||||||RI543763\r"
                 + "PID|1||12345^^^^MR||TestPatient^Jane^^^^^L||||||\r"
-                + "ORC|RE||197027|||||||^Clerk^Myron||MD67895^Pediatric^MARY^^^^MD^^RIA|||||RI2050\r"
+                + "ORC|RE||197027|||||||^Clerk^Myron|||||||RI2050\r"
                 + "RXA|0|1|20130531|20130531|48^HIB PRP-T^CVX|0.5|ML^^ISO+||00^new immunization record^NIP001|^Sticker^Nurse|^^^RI2050||||33k2a|20131210|PMC^sanofi^MVX|||CP|A\r"
                 + "RXR|C28161^IM^NCIT^IM^INTRAMUSCULAR^HL70162|RT^right thigh^HL70163\r"
                 + "OBX|1|CE|64994-7^vaccine fund pgm elig cat^LN|1|V02^VFC eligible Medicaid/MedicaidManaged Care^HL70064||||||F|||20130531|||VXC40^per imm^CDCPHINVS\r";
@@ -109,7 +109,7 @@ public class ImmunizationTest {
         assertThat(resource.getIdentifier().get(0).getValue()).isEqualTo("48-CVX");
         assertThat(resource.getIdentifier().get(0).getSystem()).isEqualTo("urn:id:extID");
         assertThat(resource.getVaccineCode().getCoding().get(0).getSystem())
-                .isEqualTo("http://hl7.org/fhir/sid/cvx");
+                .isEqualTo("http://hl7.org/fhir/sid/cvx"); // this is RXA.5 Should if  system is incorrect it  returns urn:extId:incorrectSystem....is this the  behavior  we want?
         assertThat(resource.getVaccineCode().getCoding().get(0).getCode()).isEqualTo("48");
         assertThat(resource.getVaccineCode().getText()).isEqualTo("HIB PRP-T");
         assertThat(resource.getOccurrence()).hasToString("DateTimeType[2013-05-31]");
@@ -122,10 +122,9 @@ public class ImmunizationTest {
         assertThat(resource.getLotNumber()).isEqualTo("33k2a");
         assertThat(resource.getExpirationDate()).isEqualTo("2013-12-10");
 
-        assertThat(resource.getPerformer().get(0).getFunction().getCoding().get(0).getCode())
-                .isEqualTo("OP");
+        assertThat(resource.getPerformer().get(0).getFunction().getCodingFirstRep().getCode()).isEqualTo("AP"); //RXA.10
         assertThat(resource.getPerformer().get(0).getFunction().getText())
-                .isEqualTo("Ordering Provider");
+                .isEqualTo("Administering Provider");// back  up value to ORC.12
         assertThat(resource.getPerformer().get(0).getActor().isEmpty()).isFalse();
 
         assertThat(resource.getManufacturer().isEmpty()).isFalse();
