d49443b26533a870e92005270c0d6adacbefebaf
Brian Cragun <cragun@us.ibm.com>
2021-11-03 20:16:15 -0500

    Pract refs NTE.4; fix meta corrupt; tests
    Signed-off-by: Brian Cragun <cragun@us.ibm.com>

--- src/test/java/io/github/linuxforhealth/hl7/segments/Hl7NoteFHIRConverterTest.java
+++ src/test/java/io/github/linuxforhealth/hl7/segments/Hl7NoteFHIRConverterTest.java
@@ -14,8 +14,11 @@ import java.util.Map;
 
 import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
 import org.hl7.fhir.r4.model.Condition.ConditionEvidenceComponent;
+import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Condition;
+import org.hl7.fhir.r4.model.Extension;
 import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Practitioner;
 import org.hl7.fhir.r4.model.Resource;
 import org.hl7.fhir.r4.model.ResourceType;
 import org.hl7.fhir.r4.model.ServiceRequest;
@@ -26,23 +29,27 @@ import io.github.linuxforhealth.hl7.segments.util.ResourceUtils;
 public class Hl7NoteFHIRConverterTest {
 
     // Tests NTE creation for OBX (Observations) and ORC/OBRs (ServiceRequests)
+    // Test associated Practitioners and references created. (NTE.4)
     @Test
     public void testNoteCreationMutipleOBX() {
         String hl7ORU = "MSH|^~\\&|||||20180924152907||ORU^R01^ORU_R01|213|T|2.3.1|||||||||||\n"
-                + "PID|||1234^^^^MR||DOE^JANE^|||F||||||||||||||||||||||\n"
+                + "PID|||Pract1ID^^^^MR||DOE^JANE^|||F||||||||||||||||||||||\n"
                 // TODO: Future work, handle NTE's on PID
                 // "NTE|1|O|TEST NOTE DD line 1|\n" + "NTE|2|O|TEST NOTE DD line 2 |\n" + "NTE|3|O|TEST NOTE D line 3|\n"  
                 + "PV1|1|I||||||||||||||||||||||||||||||||||||||||||20180924152707|\n"
                 + "ORC|RE|248648498^|248648498^|ML18267-C00001^Beaker|||||||||||||||||||||||||||\n"
                 + "OBR|1|248648498^|248648498^|83036E^HEMOGLOBIN A1C^PACSEAP^^^^^^HEMOGLOBIN A1C||||||||||||||||||||||||||||||||||||\n"
-                + "NTE|1|O|TEST ORC/OBR NOTE AA line 1|\n" 
+                // ServiceRequest NTE has a practitioner reference in NTE.4
+                + "NTE|1|O|TEST ORC/OBR NOTE AA line 1||Pract1ID^Pract1Last^Pract1First|\n" 
                 + "NTE|2|O|TEST NOTE AA line 2|\n"
                 + "NTE|3|O|TEST NOTE AA line 3|\n"
                 + "OBX|1|NM|17985^GLYCOHEMOGLOBIN HGB A1C^LRR^^^^^^GLYCOHEMOGLOBIN HGB A1C||5.6|%|<6.0||||F||||||||||||||\n"
+                // GLYCOHEMOGLOBIN Observation NTE has a practitioner reference in NTE.4.  Note in second NTE.
                 + "NTE|1|L|TEST OBXa NOTE BB line 1|\n" 
-                + "NTE|2|L|TEST NOTE BB line 2|\n"
+                + "NTE|2|L|TEST NOTE BB line 2||Pract2ID^Pract2Last^Pract2First|\n"
                 + "NTE|3|L|TEST NOTE BB line 3|\n"
                 + "OBX|2|NM|17853^MEAN BLOOD GLUCOSE^LRR^^^^^^MEAN BLOOD GLUCOSE||114.02|mg/dL|||||F||||||||||||||\n"
+                // Glucose Observation NTE has no practitioner reference in NTE.4
                 + "NTE|1|L|TEST OBXb NOTE CC line 1|\n" 
                 + "NTE|2|L|TEST NOTE CC line 2|\n"
                 + "NTE|3|L|TEST NOTE CC line 3|\n"
@@ -60,8 +67,12 @@ public class Hl7NoteFHIRConverterTest {
                 ResourceUtils.context);
         assertThat(serviceRequest.hasNote()).isTrue();
         assertThat(serviceRequest.getNote()).hasSize(1);
+        // Processing adds "  \n" two spaces and a line feed
+        // Note on test strings. Must double back-slashes to create single backslash in string.
         assertThat(serviceRequest.getNote().get(0).getText())
                 .isEqualTo("TEST ORC/OBR NOTE AA line 1  \\nTEST NOTE AA line 2  \\nTEST NOTE AA line 3  \\n");
+        assertThat(serviceRequest.getNote().get(0).hasAuthorReference()).isTrue();
+        String practitionerServReqRefId = serviceRequest.getNote().get(0).getAuthorReference().getReference();
 
         // Two observations.  One has GLYCOHEMOGLOBIN and notes BB, One has GLUCOSE and notes CC
         List<Resource> observations = ResourceUtils.getResourceList(e, ResourceType.Observation);
@@ -74,16 +85,48 @@ public class Hl7NoteFHIRConverterTest {
             obsGlucose = obsHemoglobin;
             obsHemoglobin = temp;
         }
-        assertThat(obsGlucose.hasNote()).isTrue();
-        assertThat(obsGlucose.getNote()).hasSize(1);
-        // Processing adds "  \n" two spaces and a line feed
-        // Note on test strings. Must double back-slashes to create single backslash in string.
-        assertThat(obsGlucose.getNote().get(0).getText()).isEqualTo(
-                "TEST OBXb NOTE CC line 1  \\nTEST NOTE CC line 2  \\nTEST NOTE CC line 3  \\nTEST NOTE CC line 4  \\n");
+        // Validate the note contents and references 
         assertThat(obsHemoglobin.hasNote()).isTrue();
         assertThat(obsHemoglobin.getNote()).hasSize(1);
         assertThat(obsHemoglobin.getNote().get(0).getText())
                 .isEqualTo("TEST OBXa NOTE BB line 1  \\nTEST NOTE BB line 2  \\nTEST NOTE BB line 3  \\n");
+        assertThat(obsHemoglobin.getNote().get(0).hasAuthorReference()).isTrue();  
+        String practitionerObsHemoglobinRefId = obsHemoglobin.getNote().get(0).getAuthorReference().getReference();   
+
+        assertThat(obsGlucose.hasNote()).isTrue();
+        assertThat(obsGlucose.getNote()).hasSize(1);
+        assertThat(obsGlucose.getNote().get(0).getText()).isEqualTo(
+                "TEST OBXb NOTE CC line 1  \\nTEST NOTE CC line 2  \\nTEST NOTE CC line 3  \\nTEST NOTE CC line 4  \\n");
+        assertThat(obsGlucose.getNote().get(0).hasAuthorReference()).isFalse();
+     
+
+        // Two Practitioners, one for the serviceRequest, one for the GLYCOHEMOGLOBIN Observation
+        List<Resource> practitioners = ResourceUtils.getResourceList(e, ResourceType.Practitioner);
+        assertThat(practitioners).hasSize(2);
+        Practitioner practitionerServReq = ResourceUtils.getResourcePractitioner(practitioners.get(0),
+                ResourceUtils.context);
+        Practitioner practitionerObsHemoglobin = ResourceUtils.getResourcePractitioner(practitioners.get(1),
+                ResourceUtils.context);
+        if (practitionerServReq.getIdentifierFirstRep().getValue() != "Pract1ID") {
+                Practitioner temp = practitionerObsHemoglobin;
+                practitionerObsHemoglobin = practitionerServReq;
+                practitionerServReq = temp;
+        }
+        // Check the values for the Practitioners and validate match to references.             
+        assertThat(practitionerServReq.getIdentifier()).hasSize(1);
+        assertThat(practitionerServReq.getIdentifierFirstRep().getValue()).isEqualTo("Pract1ID");
+        assertThat(practitionerServReq.getName()).hasSize(1);
+        assertThat(practitionerServReq.getNameFirstRep().getText()).isEqualTo("Pract1First Pract1Last");
+        assertThat(practitionerServReq.getId()).isEqualTo(practitionerServReqRefId);   // Check the cross-reference
+        // Sanity check to confirm data corruption in meta content has not returned.
+        CodeableConcept ccSourceEventTrigger = (CodeableConcept)practitionerServReq.getMeta().getExtensionByUrl("http://ibm.com/fhir/cdm/StructureDefinition/source-event-trigger").getValue();
+        assertThat(ccSourceEventTrigger.hasText()).isFalse(); 
+        
+        assertThat(practitionerObsHemoglobin.getIdentifier()).hasSize(1);
+        assertThat(practitionerObsHemoglobin.getIdentifierFirstRep().getValue()).isEqualTo("Pract2ID");
+        assertThat(practitionerObsHemoglobin.getName()).hasSize(1);
+        assertThat(practitionerObsHemoglobin.getNameFirstRep().getText()).isEqualTo("Pract2First Pract2Last");
+        assertThat(practitionerObsHemoglobin.getId()).isEqualTo(practitionerObsHemoglobinRefId);   // Check the cross-reference  
 
     }
 
@@ -119,9 +162,7 @@ public class Hl7NoteFHIRConverterTest {
                 + "NTE|2|L|TEST NOTE EE line 2|\n"
                 + "NTE|3|L|TEST NOTE EE line 3|\n"
                 + "OBX|2|NM|7302^Resp Rate|1|19||||||F|||20180520230000|||\n"
-                + "NTE|1|L|TEST OBXf NOTE FF line 1|\n" 
-                + "NTE|2|L|TEST NOTE FF line 2|\n"
-                + "NTE|3|L|TEST NOTE FF line 3|\n";
+                + "NTE|1|L|TEST OBXf NOTE FF line 1|\n";
                 // TODO:  have one of these have a single NTE segment, to verify that is handled correctly (no newlines added to text).
 
         List<BundleEntryComponent> e = ResourceUtils.createFHIRBundleFromHL7MessageReturnEntryList(hl7message);
@@ -157,7 +198,7 @@ public class Hl7NoteFHIRConverterTest {
         matchObsCodeToNotes.put("17985", "TEST OBXb NOTE BB line 1  \\nTEST NOTE BB line 2  \\nTEST NOTE BB line 3  \\n");
         matchObsCodeToNotes.put("17853", "TEST OBXc NOTE CC line 1  \\nTEST NOTE CC line 2  \\nTEST NOTE CC line 3  \\n");
         matchObsCodeToNotes.put("8595", "TEST OBXe NOTE EE line 1  \\nTEST NOTE EE line 2  \\nTEST NOTE EE line 3  \\n");
-        matchObsCodeToNotes.put("7302", "TEST OBXf NOTE FF line 1  \\nTEST NOTE FF line 2  \\nTEST NOTE FF line 3  \\n");
+        matchObsCodeToNotes.put("7302", "TEST OBXf NOTE FF line 1");
 
         // This map tells us what Parent should be associated with an Observation code
         Map<String, String> matchObsCodeToParent = new HashMap<>();
