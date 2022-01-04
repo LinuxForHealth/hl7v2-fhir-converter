59d7840ab0293dc27912be53a16d21f0c8950df0
jared.grant@ibm.com <jared.grant@ibm.com>
2022-01-04 18:02:19 -0500

    fix immunization.education
    Signed-off-by: jared.grant@ibm.com <jared.grant@ibm.com>

--- src/test/java/io/github/linuxforhealth/hl7/segments/Hl7ImmunizationFHIRConversionTest.java
+++ src/test/java/io/github/linuxforhealth/hl7/segments/Hl7ImmunizationFHIRConversionTest.java
@@ -370,21 +370,24 @@ class Hl7ImmunizationFHIRConversionTest {
     @Test
     void testEducation() throws IOException {
 
-
         String hl7VUXmessageRep = "MSH|^~\\&|MYEHR2.5|RI88140101|KIDSNET_IFL|RIHEALTH|20130531||VXU^V04^VXU_V04|20130531RI881401010105|P|2.5.1|||NE|AL||||||RI543763\r"
                 + "PID|1||12345^^^^MR||TestPatient^Jane^^^^^L||||||\r"
                 + "ORC|RE||197027||CP|||||^Clerk^Myron|||||||RI2050\r"
                 + "RXA|0|1|20130531|20130531|48^HIB PRP-T^CVX|0.5|ML^^^||00^new immunization record^NIP001|^Sticker^Nurse|^^^RI2050||||33k2a|20131210|PMC^sanofi^MVX|00^Patient refusal^NIP002|||A\r"
                 + "RXR|C28161^IM^NCIT^IM^INTRAMUSCULAR^HL70162|RT^right thigh^HL70163\r"
                 + "OBX|1|DTM|29768-9^VIS Publication Date^LN|1|19981216||||||F|||20130531|||VXC40^per imm^CDCPHINVS\r"
-                + "OBX|2|ST|69764-9^HIB Info Sheet^LN|1|PDF||||||F|||20130531|||VXC40^per imm^CDCPHINVS\r";
+                + "OBX|2|ST|69764-9^HIB Info Sheet^LN|1|PDF||||||F|||20130531|||VXC40^per imm^CDCPHINVS\r"
+                + "OBX|3|DTM|29769-7^Date vaccine information statement presented^LN|1|19991216||||||F|||20130531|||VXC40^per imm^CDCPHINVS\r";
 
         Immunization immunization = ResourceUtils.getImmunization(hl7VUXmessageRep);
 
         //Education.DocumentType (OBX.5) shows under the condition that OBX.3.1 has 69764-9 as the code
-        assertThat(immunization.getEducation().get(1).getDocumentType()).isEqualTo("PDF");
+        assertThat(immunization.getEducation().get(1).getDocumentType()).isEqualTo("unspecified");
 
         //Education.PublicationDate shows under the condition that OBX.3.1 has 29768-9 as the code
-        assertThat(immunization.getEducationFirstRep().hasPublicationDate()).isTrue();
+        assertThat(immunization.getEducationFirstRep().getPublicationDateElement().toString()).contains("1998-12-16");
+
+        //Education.PresentationDate shows under the condition that OBX.3.1 has 29769-7 as the code
+        assertThat(immunization.getEducation().get(2).getPresentationDateElement().toString()).contains("1999-12-16");
     }
 }
--- src/test/java/io/github/linuxforhealth/hl7/segments/util/ResourceUtils.java
+++ src/test/java/io/github/linuxforhealth/hl7/segments/util/ResourceUtils.java
@@ -50,7 +50,6 @@ public class ResourceUtils {
     public static List<BundleEntryComponent> createFHIRBundleFromHL7MessageReturnEntryList(String inputSegment) {
         HL7ToFHIRConverter ftv = new HL7ToFHIRConverter();
         String json = ftv.convert(inputSegment, OPTIONS);
-        System.out.println(json);
         assertThat(json).isNotBlank();
         LOGGER.debug("FHIR json result:\n" + json);
         FHIRContext context = new FHIRContext();
