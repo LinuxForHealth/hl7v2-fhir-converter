597f9d6ce824ccd5b095a8de49dcbf1b2519fd2e
Stuart McGrigor <stuart.mcgrigor@midcentraldhb.govt.nz>
2023-08-22 10:42:24 +1200

    Added toString() to SimpleVariable - it improves debugging output

--- src/main/java/io/github/linuxforhealth/hl7/expression/variable/SimpleVariable.java
+++ src/main/java/io/github/linuxforhealth/hl7/expression/variable/SimpleVariable.java
@@ -67,6 +67,10 @@ public class SimpleVariable implements Variable {
     return name;
   }
 
+  @Override
+  public String toString() {
+    return name + "=" + spec.toString();   // let's improve debugging
+  }
 
   // resolve variable value
 
