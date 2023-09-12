d8eb5cd660a05e2cf71a9b267d0af6676eaf9290
Stuart McGrigor <stuart.mcgrigor@midcentraldhb.govt.nz>
2023-09-12 15:09:13 +1200

    When parsing an HL7 specification, any segment that begins with Z is a valid segment name; PID.5.1.2 is a valid hl7 specification, and it includes SubComponent
    Signed-off-by: Stuart McGrigor <stuart.mcgrigor@midcentraldhb.govt.nz>

--- src/main/java/io/github/linuxforhealth/hl7/expression/specification/SpecificationParser.java
+++ src/main/java/io/github/linuxforhealth/hl7/expression/specification/SpecificationParser.java
@@ -33,13 +33,20 @@ public class SpecificationParser {
     if (stk.hasNext()) {
       String tok = stk.next();
 
-      if (SupportedSegments.contains(tok)) {
+      // tokens that start with Z are also valid SEGMENTS
+      if (SupportedSegments.contains(tok) || tok.startsWith("Z")) {
         segment = tok;
         if (stk.hasNext()) {
           field = stk.nextToken();
         }
         if (stk.hasNext()) {
           component = NumberUtils.toInt(stk.nextToken());
+        }
+
+        // Don't forget the SubComponents - PID.5.1.2 & PID.5.1.3, can be quite useful
+        //   - allowing us to separate the 'van' and 'van der' prefixes from names like Ludwig van Beethoven, and Cornelius van der Westhuizen
+        if (stk.hasNext()) {
+          subComponent = NumberUtils.toInt(stk.nextToken());
         }
       } else {
         field = tok;
